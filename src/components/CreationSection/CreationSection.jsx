0ea07f18ede82f20f605ac8d6ad92baa869fcf6a
Steno-95 <lezzistefano95@gmail.com>
2025-05-16 17:53:42 +0200

    Fix:create layout for mobile

--- src/components/CreationSection/CreationSection.jsx
+++ src/components/CreationSection/CreationSection.jsx
@@ -12,9 +12,11 @@ const content = [
 ];
 function CreationSection() {
   return (
-    <section className="p-8 grid grid-cols-1">
-      <h2 className="row-start-1">Our creations</h2>
-      <button className="order-2"> See all</button>
+    <section className="p-8 grid grid-cols-1 justify-items-center gap-10">
+      <h2 className="row-start-1 uppercase text-[2.5rem]">Our creations</h2>
+      <button className="order-2 uppercase text-[1.5rem] border-2 w-fit py-3 px-15 tracking-widest">
+        See all
+      </button>
       <div className="gallery grid grid-cols-1 gap-8">
         {content.map((item) => (
           <Card title={item.title} imgSrc={item.img} key={item.img} />
