311ef9c51a3318a43249d1a45b9edfc6bf753666
Steno-95 <lezzistefano95@gmail.com>
2025-05-16 18:47:39 +0200

    Style:remove wrong class

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,17 @@
 import CreationSection from "./components/CreationSection/CreationSection";
 import InteractiveSection from "./components/InteractiveSection/InteractiveSection";
 import Header from "./ui/Header";
+import SiteFooter from "./ui/SiteFooter";
 
 function App() {
   return (
-    <div className="container">
+    <div>
       <Header />
       <main className="">
         <InteractiveSection />
         <CreationSection />
       </main>
-      <footer className="bg-(--black) text-(--white) grid grid-cols-1 items-center justify-items-center gap-5 py-15">
-        <img src="/images/logo.svg" />
-        <nav className="py-4">
-          <ul className="flex flex-col gap-5">
-            <li>About</li>
-            <li>Careers</li>
-            <li>Events</li>
-            <li>Products</li>
-            <li>Support</li>
-          </ul>
-        </nav>
-        <ul className="flex gap-5">
-          <li>
-            <img src="/images/icon-facebook.svg" />
-          </li>
-          <li>
-            <img src="/images/icon-twitter.svg" />
-          </li>
-          <li>
-            <img src="/images/icon-pinterest.svg" />
-          </li>
-          <li>
-            <img src="/images/icon-instagram.svg" />
-          </li>
-        </ul>
-        <p className="text-(--grey)">
-          © 2021 Loopstudios. All rights reserved.
-        </p>
-      </footer>
+      <SiteFooter />
     </div>
   );
 }
