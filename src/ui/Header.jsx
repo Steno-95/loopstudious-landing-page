c1d0e1e948f87330f42986274b192047f5ae2758
Steno-95 <lezzistefano95@gmail.com>
2025-05-16 17:02:39 +0200

    Refactor:refactor the header as a component

---
+++ src/ui/Header.jsx
@@ -0,0 +1,30 @@
+function Header() {
+  return (
+    <header className="bg-[url(/images/mobile/image-hero.jpg)] lg:bg-[url(/images/desktop/image-hero.jpg)] min-h-screen p-5 bg-top py-8 flex flex-col bg-no-repeat bg-cover">
+      <nav className="flex justify-between">
+        <figure>
+          <img src="/images/logo.svg" />
+        </figure>
+        {window.innerWidth >= 768 && (
+          <ul className="flex text-(--white) gap-3">
+            <li>About</li>
+            <li>Careers</li>
+            <li>Events</li>
+            <li>Products</li>
+            <li>Support</li>
+          </ul>
+        )}
+        {window.innerWidth < 768 && (
+          <img src="/images/icon-hamburger.svg" className="size-7" />
+        )}
+      </nav>
+      <div className="min-h-svh place-content-center">
+        <h1 className="text-(--white) uppercase text-6xl p-8 border-2">
+          Immersive experiences that deliver
+        </h1>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
