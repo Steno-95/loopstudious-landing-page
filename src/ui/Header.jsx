16aab5c6c42a911a0bc2d7103a24190e73aab23b
Steno-95 <lezzistefano95@gmail.com>
2025-05-20 14:12:32 +0200

    Feat: add basic layout for mobile menu

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,28 +1,49 @@
+import { useEffect, useState } from "react";
+import Navigation from "./Navigation";
+import Logo from "./Logo";
+const content = ["about", "careers", "events", "products", "support"];
 function Header() {
+  const [isShowing, setIsShowing] = useState(false);
+  const screenSize = window.innerWidth;
+  // useEffect(()=> {}, [screenSize]);
+  console.log(window.matchMedia(`(max-width: ${screenSize}px)`));
   return (
-    <header className="bg-[url(/images/mobile/image-hero.jpg)] lg:bg-[url(/images/desktop/image-hero.jpg)] min-h-screen px-5 bg-top py-8 flex flex-col bg-no-repeat bg-cover">
-      <nav className="flex justify-between">
-        <figure>
-          <img src="/images/logo.svg" />
-        </figure>
-        {window.innerWidth >= 768 && (
-          <ul className="flex text-(--white) gap-3">
-            <li>About</li>
-            <li>Careers</li>
-            <li>Events</li>
-            <li>Products</li>
-            <li>Support</li>
-          </ul>
-        )}
-        {window.innerWidth < 768 && (
-          <img src="/images/icon-hamburger.svg" className="size-7" />
-        )}
-      </nav>
-      <div className="min-h-svh place-content-center">
-        <h1 className="text-(--white) uppercase text-[2.5rem] p-8 border-2">
-          Immersive experiences that deliver
-        </h1>
+    <header className="header header-medias relative">
+      <Navigation
+        isShowing={isShowing}
+        onClick={setIsShowing}
+        content={content}
+        screenSize={screenSize}
+      />
+      <div className="hero-box">
+        <h1 className="hero-text">Immersive experiences that deliver</h1>
       </div>
+      {isShowing && (
+        <div className="fixed top-1/2 left-1/2 w-screen h-screen bg-black/90 -translate-[50%] grid grid-rows-[min-content_1fr] items-center p-10 text-white just">
+          <div className="flex justify-between items-center">
+            <Logo />
+            <button
+              className="nav-btn"
+              onClick={() => setIsShowing((show) => !show)}
+            >
+              <svg className="size-6" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M17.778.808l1.414 1.414L11.414 10l7.778 7.778-1.414 1.414L10 11.414l-7.778 7.778-1.414-1.414L8.586 10 .808 2.222 2.222.808 10 8.586 17.778.808z"
+                  fill="#FFF"
+                  fillRule="evenodd"
+                />
+              </svg>
+            </button>
+          </div>
+          <ul className="flex flex-col gap-10 text-[2.2rem] uppercase">
+            <li>About</li>
+            <li>careers</li>
+            <li>events</li>
+            <li>products</li>
+            <li>support</li>
+          </ul>
+        </div>
+      )}
     </header>
   );
 }
